289fc3c9c42f50fd31b219b06c624e937744b14b
Nick James <nxj2348@rit.edu>
2016-10-19 18:54:55 -0400

    added unit testing dependencies

---
+++ app/src/androidTest/java/com/nickjames/polychromatictuner/SelectTuningActivityTest.java
@@ -0,0 +1,14 @@
+package com.nickjames.polychromatictuner;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Created by Nick on 10/19/2016.
+ */
+@RunWith(AndroidJUnit4.class)
+public class SelectTuningActivityTest {
+
+
+}
